71423f8c27539f6c66ff77f68960c170b754c3ce
henderiw <wim.henderickx@gmail.com>
2020-12-23 10:56:28 +0100

    Update netlink.go

--- pkg/nlink/netlink.go
+++ pkg/nlink/netlink.go
@@ -12,7 +12,14 @@ func GetLinks() {
 		log.Error(err)
 	}
 	for _, l := range ll {
-		log.Infof("Link Info: %v", l)
+		attr := l.Attrs()
+		log.Infof("Link Name: %s", attr.Name)
+		log.Infof("  Link Type: %v", l.Type())
+		log.Infof("  Link Attr: %v", attr)
+		for _, v := range attr.Vfs {
+			log.Infof("    VFs: %v", v)
+
+		}
 	}
 
 }
